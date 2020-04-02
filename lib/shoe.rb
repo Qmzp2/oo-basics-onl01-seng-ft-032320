defaf720d71ae9d69b2c87423a7f50370c6ece27
Kevin Mathias <komathias1@gmail.com>
2020-04-02 05:51:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@
 class Shoe
  
  attr_accessor :brand, :color, :size, :material, :condition
-attr_reader :brand, :color, :size, :material :condition
+attr_reader :brand, :color, :size, :material, :condition
  
  def initialize(brand)
  @brand = brand
